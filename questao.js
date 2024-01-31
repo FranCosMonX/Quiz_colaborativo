ecf60bd31d838376dc30f851876c360b487500d7
Alex Costa <thg6321@gmail.com>
2024-01-31 02:12:10 -0300

    v1

---
+++ questao.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    questao: "<br>Um século tem quantos anos ?<br> ",
+    respostas: [
+      { option: "a) 70", correct: false },
+      { option: "b) 100", correct: true },
+      { option: "c) 1000", correct: false },
+      { option: "d) 1500", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Que fruta é ressecada para se tornar uma ameixa seca ?",
+    respostas: [
+      { option: "a) Uva", correct: false },
+      { option: "b) Ameixa", correct: true },
+      { option: "c) Pêssego", correct: false },
+      { option: "d) Melão", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Quantas folhas tem um trevo da sorte ?",
+    respostas: [
+      { option: "a) Três", correct: false },
+      { option: "b) Cinco", correct: false },
+      { option: "c) Quatro", correct: true },
+      { option: "d) Dois", correct: false },
+    ],
+  },
+  {
+    questao: "<br>O que é a Via Láctea ?",
+    respostas: [
+      { option: "a) Marca de Leite", correct: false },
+      { option: "b) Civilização Antiga", correct: false },
+      { option: "c) Galáxia", correct: true },
+      { option: "d) Marca de Relógio", correct: true },
+    ],
+  },
+  {
+    questao: "<br>Que personagem da turma do Chaves vive cobrando o aluguel do Seu Madruga ?",
+    respostas: [
+      { option: "a) Seu Barriga", correct: true },
+      { option: "b) Kiko", correct: false },
+      { option: "c) Chaves", correct: false },
+      { option: "d) Professor Girafales", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Quem é o pai da Chiquinha ?",
+    respostas: [
+      { option: "a) Seu Madruga", correct: true },
+      { option: "b) Seu Barriga", correct: false },
+      { option: "c) Nhonho", correct: false },
+      { option: "d) Professor Girafales", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Que personagem dos quadrinhos usa um coelho como arma e tem dentes grandes ?",
+    respostas: [
+      { option: "a) Monica", correct: true },
+      { option: "b) Cebolinha", correct: false },
+      { option: "c) Cascão", correct: false },
+      { option: "d) Magali", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Que objeto usado na chuva o Pinguim do Batman usa como arma ?",
+    respostas: [
+      { option: "a) Guarda-Chuva", correct: true },
+      { option: "b) Galocha", correct: false },
+      { option: "c) Capa", correct: false },
+      { option: "d) Chapéu", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Qual a profissão dos Sete Anões ?",
+    respostas: [
+      { option: "a) Mineradores", correct: true },
+      { option: "b) Lenhadores", correct: false },
+      { option: "c) Agricultores", correct: false },
+      { option: "d) Pastores", correct: false },
+    ],
+  },
+  {
+    questao: "<br>Quantos centavos tem um real ?",
+    respostas: [
+      { option: "a) 10", correct: false },
+      { option: "b) 60", correct: false },
+      { option: "c) 50", correct: false },
+      { option: "d) 100", correct: true },
+    ],
+  },
+  
+];
